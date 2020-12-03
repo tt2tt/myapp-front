9f78fdecfa1222cc996db653dc4598bb1acb7cef
taro <groveleffect@gmail.com>
2020-12-03 12:34:34 +0900

    show_users_index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,56 @@
 <template>
   <div>
-    <!-- イベントの追加 getMsgメソッドの実行 -->
-    <button
-      type="button"
-      name="button"
-      @click="getMsg"
-    >
-      RailsからAPIを取得する
-    </button>
-    <div
-      v-for="(msg, i) in msgs"
-      :key="i"
-    >
-      {{ msg }}
+    <h2>
+      Usersテーブルの取得
+    </h2>
+    <table v-if="users.length">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>email</th>
+          <th>created_at</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(user, i) in users"
+          :key="`user-${i}`"
+        >
+          <td>{{ user.id }}</td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ dateFormat(user.created_at) }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div v-else>
+      ユーザーが取得できませんでした
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      // データの定義
-      msgs: []
-    }
+  // 非同期処理完了後値(promise)を返す async
+  // APIからデータを取得 asyncData
+  async asyncData ({ $axios }) {
+    let users = []
+    // promise確定後処理を実行
+    await $axios.$get('/api/v1/users')
+      .then(res => (users = res))
+    return { users }
   },
-  methods: {
-    // API通信の実行
-    getMsg () {
-      // 成功時の処理
-      this.$axios.$get('/api/v1/hello')
-      // msgs配列に返り値を追加
-        .then(res => this.msgs.push(res))
+  computed: {
+    // 日付のフォーマットを変換
+    dateFormat () {
+      return (date) => {
+        const dateTimeFormat = new Intl.DateTimeFormat(
+          'ja', { dateStyle: 'medium', timeStyle: 'short' }
+        )
+        return dateTimeFormat.format(new Date(date))
+      }
     }
   }
 }
